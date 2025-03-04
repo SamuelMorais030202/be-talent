5430cd12dc17e3e9c21a83272279d4c626b541b2
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-04 10:43:02 -0300

    feat: adicionando o cabeçalho da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header"
+
 export function App() {
   return (
-    <div>
+    <>
+      <Header />
+
       <h1>Iniciando desafio</h1>
-    </div>
+    </>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <header
+      role="banner"
+      className="bg-white w-full h-16 flex items-center px-4 shadow-[0px_2px_8px_0px_rgba(0,0,0,0.2)]"
+    >
+      <img
+        src="/Logo.png"
+        alt="Logo da Be Talent"
+        className="w-24 h-9"
+      />
+    </header>
+  )
+}
